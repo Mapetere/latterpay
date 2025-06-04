9b34acfa1f9f80e1606a471f3c0cd4705711f9a5
Nyasha Praise Mapetere <mapeterenyasha@gmail.com>
2025-06-04 15:01:47 +0200

    fix bugs

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from apscheduler.schedulers.background import BackgroundScheduler
 import os
 from datetime import datetime, timedelta
 import json
-from services import whatsapp
+from services.pygwan_whatsapp import whatsapp
 from services.config import sessions, donation_types, CUSTOM_TYPES_FILE, PAYMENTS_FILE
+from services import cleanuupexpired  
+from cleanupexpired import cleanup_expired_donation_types
+from services import setup_scheduled_reports
+
 
 app = Flask(__name__)
 
